cd1d14a859d9c7266a7884c93875e593669f527a
Lars Melchior <lars.melchior@gmail.com>
2016-02-25 13:05:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     zip_safe=False,
 
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7'
     ],
