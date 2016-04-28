683779fef884c89f3de6b167cc2aa503bdaca22d
Lars <lars.melchior@gmail.com>
2016-04-28 12:33:08 +0200

    changes status for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from glob import glob
 
 setup(
     name='expresso',
-    version='0.1.5a2',
+    version='0.1.5',
     description='A symbolic expression manipulation library.',
     license='MIT',
     author='Lars Melchior',
